01e0b0b6e993780ee874180a6d73ab2d0145b7a5
maria <maria.alonso158@alu.ulpgc.es>
2023-09-28 20:08:34 +0100

    added HundredthPrime requirement

--- 2. Flow Control/Loops/2.5 While Using Modulus/src/WhileUsingModulus.java
+++ 2. Flow Control/Loops/2.5 While Using Modulus/src/WhileUsingModulus.java
@@ -14,5 +14,6 @@ public class WhileUsingModulus
          }
          num++;
       }
+      System.out.println(count);
    }
 }
--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -3,5 +3,6 @@ public class HundredthPrime
    public static void main(String[] args)
    {
       /* Make a code that displays the 100th prime number */
+      //TODO finish the activity
    }
 }
